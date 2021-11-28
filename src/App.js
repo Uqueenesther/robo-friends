1b577c909e37f47ee6f11dd6e964c01cdff2cd97
Queenesther <umohqueenesther@gmail.com>
2021-11-28 13:17:54 -0800

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React {Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox';
+import { Component } from 'react';
 
-const App = () =>{
-    return(
-        <CardList robots = {robots}/>
-    )
+
+const state = {
+    robot: robots,
+    searchfield: ''
+}
+
+class App extends Component {
+    render() {
+        return(
+            <div className= 'tc'> 
+                <h1> RoboFriends</h1>
+                <SearchBox />
+                <CardList robots = {robots}/>
+            </div>
+          
+        );
+    }
+    
 }
 
 export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const SearchBox = () =>{
+    return (
+        <div className ='pa3' >
+             <input  className = 'pa3 ba b--dark-blue bg-lighest-blue'
+                type = 'search' 
+                placeholder = 'search Robofriends'
+             />
+        </div>
+       
+    )
+}
+
+export default SearchBox;
